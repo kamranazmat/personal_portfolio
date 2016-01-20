52131c39a650f115f6bd9ec5fecc89a9b21e342a
Kamran Azmat <azmat.kamran@gmail.com>
2016-01-20 16:03:36 +0530

    almost complete

--- template/about.php
+++ template/about.php
@@ -63,6 +63,6 @@
   </div>
  <div class="col-md-12"></br></div>
   <div class="col-md-4"></div>
-  <div class="col-md-6" align="right" style="color: white">
+  <div class="col-md-4" align="right" style="color: white">
   	<button class="btn btn-default" onclick="window.open('resume.php', '_blank');" style="font-family: Courier; color: #0089e6">Resume`</a></button>
   </div>
